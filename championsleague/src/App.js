059d58b8c795f53011e182c7ba5fe7033f936968
aadrsh26288 <78693934+aadrsh26288@users.noreply.github.com>
2023-02-03 11:48:38 +0530

    champions league

---
+++ championsleague/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Champions from './Components/Champions';
+function App() {
+  return (
+    <div className="">
+      <Champions/>
+
+    </div>
+  );
+}
+
+export default App;
